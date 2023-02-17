54775cd570f2d23789053855cd2f01f4f7dd0d30
Jaden loh <65410149+jadenlohh@users.noreply.github.com>
2023-02-17 19:41:57 +0800

    style: optimise view on large screen

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,7 +3,6 @@ import '../styles/home.css'
 import { Col, Image, Row } from 'react-bootstrap'
 import NavigationBar from '../components/NavigationBar'
 import SocialsSidebar from '../components/SocialsSidebar'
-import ProjectBanner from '../components/ProjectBanner'
 import PortraitImg from '../images/portrait.jpg'
 
 const Home = () => {
@@ -13,15 +12,15 @@ const Home = () => {
             <NavigationBar />
             <SocialsSidebar />
 
-            <div className='hero active'>
+            <div className='hero mx-auto'>
                 <Row style={{margin: 'auto'}}>
-                    <Col md='6' className='text-box'>
+                    <Col md='7' className='text-box'>
                         <h1>Hey, I'm Jaden</h1>
                         <p className='mt-3'>A year 2 student studying Information Technology at Nanyang Polytechnic</p>    
                     </Col>
 
-                    <Col md='6' className='image-box'>
-                        <Image fluid='true' src={PortraitImg} height='340' width='340' id='portrait-image' />
+                    <Col md='5' className='image-box'>
+                        <Image fluid='true' src={PortraitImg} id="portrait" />
                     </Col>
                 </Row>
             </div>
